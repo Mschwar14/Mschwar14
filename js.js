1deb95d030db0683d7f4527f489ab927bafb5738
Mschwar14 <59878312+Mschwar14@users.noreply.github.com>
2020-05-19 14:40:19 +0200

    update

--- js.js
+++ js.js
@@ -16,3 +16,28 @@ const options = {
 const darkmode = new Darkmode(options);
 
 darkmode.showWidget();
+
+
+
+var timer = new Timer();
+$('#chronoExample .startButton').click(function () {
+    timer.start();
+});
+$('#chronoExample .pauseButton').click(function () {
+    timer.pause();
+});
+$('#chronoExample .stopButton').click(function () {
+    timer.stop();
+});
+$('#chronoExample .resetButton').click(function () {
+    timer.reset();
+});
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#chronoExample .values').html(timer.getTimeValues().toString());
+});
+timer.addEventListener('started', function (e) {
+    $('#chronoExample .values').html(timer.getTimeValues().toString());
+});
+timer.addEventListener('reset', function (e) {
+    $('#chronoExample .values').html(timer.getTimeValues().toString());
+});
